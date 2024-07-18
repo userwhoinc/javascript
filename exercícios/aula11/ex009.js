c607f0355948ee2997a27ea415f9165fe389d897
sara <159207970+userwhoinc@users.noreply.github.com>
2024-07-18 17:45:03 -0300

    atualizando

---
+++ exercícios/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil') {
+    console.log('Brasileiro!! ganhou um carro')
+} else {
+    console.log('Estrangeiro fedido')
+}
---
+++ exercícios/aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 90
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60) { //condição simples 
+    console.log(`Você ultrapassou a velocidade permitida. Multado.`)
+}
+console.log(`Dirija com cuidado!`)
