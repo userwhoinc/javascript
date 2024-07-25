bfdc1b66cd2500ec24f1728b1495c3cce239e450
sara <159207970+userwhoinc@users.noreply.github.com>
2024-07-25 14:00:10 -0300

    teste de repetições parte 1

---
+++ exercícios/aula013/teste.js
@@ -0,0 +1,24 @@
+ var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+ } while(c <= 8)
+
+
+ 
+     /* 
+  var c = 1
+ while (c <= 80) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1 --> vai somar 1
+ }
+    */ 
+
+/*
+console.log('Suave')
+console.log('Suave')
+console.log('Suave')
+console.log('Suave')
+console.log('Suave')
+console.log('Suave')
+*/
