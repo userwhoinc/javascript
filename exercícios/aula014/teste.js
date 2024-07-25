2ce9ef59680f1e64d6866e71fa0c395a9abcb3aa
sara <159207970+userwhoinc@users.noreply.github.com>
2024-07-25 14:37:32 -0300

    teste parte 2 feito

---
+++ exercícios/aula014/teste.js
@@ -0,0 +1,19 @@
+/* var c = 1
+while(c <= 6) {
+    console.log(c)
+    c++
+}
+ ou
+
+ do {
+ console.log(c)
+    c++
+ } while(c <= 6) */
+
+ // --------------------
+
+ console.log('Vai começar')
+ for(var c = 1; c <= 4; c++) {
+    console.log(c)
+ }
+ console.log('FIM')
