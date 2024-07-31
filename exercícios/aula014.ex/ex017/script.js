b5ab51e6b8739b372f87e73d6941a114d94bd932
sara <159207970+userwhoinc@users.noreply.github.com>
2024-07-30 21:21:16 -0300

    exercício 017

--- exercícios/aula014.ex/ex017/script.js
+++ exercícios/aula014.ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert ('Olá')
+function contar() {
+    var num = document.getElementById('num')
+    var tab = document.getElementById('tab')
+    if(num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while(c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }        
+       
+}
