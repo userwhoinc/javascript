4d54930663a3b6b84a95369bee171cbbf4fc5fe4
sara <159207970+userwhoinc@users.noreply.github.com>
2024-08-10 16:38:02 -0300

    .

---
+++ desafio/desafio003/javascript.js
@@ -0,0 +1,11 @@
+function conferir() {
+var n = document.getElementById('num')
+var res = document.getElementById('res')
+res.innerHTML = ''
+
+if (n%2 == 0) {
+    res.innerHTML = 'Número par!!'
+} else {
+    res.innerHTML = 'Número ímpar!!'
+}
+}
